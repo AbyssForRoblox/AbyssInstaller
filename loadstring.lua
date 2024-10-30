0939a8b1e7bf970f953115fc05b6770976138cee
AbyssForRoblox <9dv8jqda@mailmask.me>
2024-10-30 07:03:00 -0700

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,2 @@
+--[[ Join our discord to get any help at abyssforroblox.github.io! --]]
 loadstring(game:HttpGet("https://raw.githubusercontent.com/AbyssForRoblox/AbyssInstaller/refs/heads/main/Installer.lua"))()
